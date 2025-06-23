1c31ea55be1d48c03d917142338a0d194f29c280
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-06-22 23:48:06 -0500

    feat: enhance Project model with UserId and timestamps

--- Q2.TeeLab/DesignLab/Domain/Model/Aggregates/Project.cs
+++ Q2.TeeLab/DesignLab/Domain/Model/Aggregates/Project.cs
@@ -1,16 +1,38 @@
-﻿using Q2.TeeLab.DesignLab.Domain.Model.ValueObjects;
+﻿using Q2.TeeLab.DesignLab.Domain.Model.Entities;
+using Q2.TeeLab.DesignLab.Domain.Model.ValueObjects;
 
 namespace Q2.TeeLab.DesignLab.Domain.Model.Aggregates;
 
 public class Project
 {
     public ProjectId Id { get; private set; }
+    public UserId UserId { get; private set; }
+    
     public string Title { get; private set; }
+    public Uri PreviewImageUrl { get; private set; }
+    
     public EGarmentColor GarmentColor { get; private set; }
     public EGarmentGender GarmentGender { get; private set; }
     public EGarmentSize GarmentSize { get; private set; }
+
+    public ICollection<Layer> Layers { get; private set; }
     
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
     
-    
-    
+    public Project(UserId userId, string title, EGarmentColor garmentColor, EGarmentGender garmentGender, EGarmentSize garmentSize)
+    {
+        Id = new ProjectId();
+        UserId = userId;
+        Title = title;
+        GarmentColor = garmentColor;
+        GarmentGender = garmentGender;
+        GarmentSize = garmentSize;
+        
+        Layers = new List<Layer>();
+        
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
 }
---
+++ Q2.TeeLab/DesignLab/Domain/Model/ValueObjects/UserId.cs
@@ -0,0 +1,8 @@
+﻿namespace Q2.TeeLab.DesignLab.Domain.Model.ValueObjects;
+
+public record UserId(Guid Id)
+{
+    public UserId() : this(Guid.NewGuid())
+    {
+    }
+}
