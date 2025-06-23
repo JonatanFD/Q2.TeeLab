73d8bac8e2428636475cf20e178aaa028f05b2f1
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-06-23 10:57:14 -0500

    feat: add ModelBuilder extensions for snake case naming convention and string utilities

---
+++ Q2.TeeLab/DesignLab/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Q2.TeeLab.DesignLab.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+public static class ModelBuilderExtensions
+{
+
+    public static void ApplyDesignLabConfiguration(this ModelBuilder modelBuilder)
+    {
+        
+    }
+    
+}
